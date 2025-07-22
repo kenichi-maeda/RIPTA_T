6398fa050c60fa62c7a9913d94ba334e188b3cd7
Kenichi Maeda <kenichi.maeda121@gmail.com>
2025-07-22 17:51:50 -0400

    Add dark mode

--- RIPTA T/RIPTA_TApp.swift
+++ RIPTA T/RIPTA_TApp.swift
@@ -11,11 +11,14 @@ import SwiftUI
 struct RIPTA_TApp: App {
     // Single shared FavoritesManager
     @StateObject private var favoritesManager = FavoritesManager()
+    
+    @AppStorage("appearance") private var appearanceRaw = Appearance.system.rawValue
 
     var body: some Scene {
         WindowGroup {
             HomeView()
               .environmentObject(favoritesManager)
+              .preferredColorScheme(Appearance(rawValue: appearanceRaw)?.colorScheme)
         }
     }
 }
